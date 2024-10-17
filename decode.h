9a6c1384f84fa9c97a2eae839b9b11e956908e6e
Avah <114538590+zambiers@users.noreply.github.com>
2024-10-16 18:22:07 -0700

    Create decode.h
    This is the start of the header file that will server as the base. This is subject to change.

---
+++ decode.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Decrypt{
+public:
+/*
+  Constructors and Desstructors
+*/
+
+  Decrypt();
+  ~Decrypt();
+
+/*
+  Main functions
+*/
+s
+  bool input_correct();
+  string encode();
+
+
+private:
+  string input;
+  string text;
+  string generate;
+  
+}
